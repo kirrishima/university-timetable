8f79a649371cacf6791f125a6e48161b85839058
kirrishima <kirrishima@users.noreply.github.com>
2026-02-04 21:27:02 +0000

    deploy: 0788c6321c8fd7b3d22051f8d5dc3d0173038874

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/university-timetable/sw.js', { scope: '/university-timetable/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
