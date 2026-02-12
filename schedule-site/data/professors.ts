ea77e641b4d431fc9abbc241b02b7a133f715708
Moonglow <85281868+kirrishima@users.noreply.github.com>
2026-02-12 10:56:15 +0300

    Update professors.ts

--- schedule-site/data/professors.ts
+++ schedule-site/data/professors.ts
@@ -43,6 +43,26 @@ export const ALL_PROFESSORS: ProfessorListEntry[] = [
                 department: 'Кафедра информационных систем и технологий',
                 imageUrl: 'https://isit.belstu.by/wp-content/uploads/2020/10/9a84cd85-8d95-4bf3-abb3-0ce4f07a14cb-300x279.jpg' 
             },
+            {
+                fullName: 'Сазонова Дарья Владимировна',
+                department: 'Кафедра информационных систем и технологий',
+                imageUrl: 'https://belstu.by/images/sotrudnikam/personal-page/sazonova.png'
+            },
+            {
+                fullName: 'Сухорукова Ирина Геннадьевна',
+                department: 'Кафедра программной инженерии',
+                imageUrl: 'https://belstu.by/images/it/pi/suxorukova-2.jpg'
+            },
+            {
+                fullName: 'Авдеева Вера Дмитриевна',
+                department: 'Кафедра программной инженерии',
+                imageUrl: 'https://belstu.by/userfolder/%D0%9A%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%B8%D0%B8/img-20250929-132948-779.jpg'
+            },
+            {
+                fullName: 'Авдеева Вера Дмитриевна',
+                department: 'Кафедра программной инженерии',
+                imageUrl: 'https://belstu.by/userfolder/%D0%9A%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%B8%D0%B8/img-20250929-132948-779.jpg
+            },
         ],
     },
 ];
