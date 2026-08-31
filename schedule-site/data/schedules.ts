d9235c6fe26d74a0e1b94334baa93a521f84a6ed
Litvinas <vaclovkacinskas@gmail.com>
2026-08-31 13:54:59 +0300

    add 4_6_1_7_2026

--- schedule-site/data/schedules.ts
+++ schedule-site/data/schedules.ts
@@ -3,6 +3,7 @@ import { FIT_3_6_1_5sem_2025_SCHEDULE } from './FIT_3_6_1_5sem_2025_SCHEDULE';
 import { FIT_3_6_1_6sem_2025_SCHEDULE } from './FIT_3_6_1_6sem_2025_SCHEDULE';
 import { FIT_3_6_2_6sem_2026_SCHEDULE } from './FIT_3_6_2_6sem_2026_SCHEDULE';
 import { FIT_4_6_2_7sem_2026_SCHEDULE } from './FIT_4_6_2_7sem_2026_SCHEDULE';
+import { FIT_4_6_1_7sem_2026_SCHEDULE } from './FIT_4_6_1_7sem_2026_SCHEDULE';
 
 
 export const ALL_SCHEDULES: ScheduleEntry[] = [
@@ -35,13 +36,23 @@ export const ALL_SCHEDULES: ScheduleEntry[] = [
     universityName: 'БГТУ',
     semester: 'Весенний'
   },
-    {
+  {
     faculty: 'Факультет информационных технологий',
     facultyShort: "ФИТ",
     course: 4,
     group: 6,
     subgroup: 2,
     schedule: FIT_4_6_2_7sem_2026_SCHEDULE,
+    universityName: 'БГТУ',
+    semester: 'Осенний'
+  },
+  {
+    faculty: 'Факультет информационных технологий',
+    facultyShort: "ФИТ",
+    course: 4,
+    group: 6,
+    subgroup: 1,
+    schedule: FIT_4_6_1_7sem_2026_SCHEDULE,
     universityName: 'БГТУ',
     semester: 'Осенний'
   }
